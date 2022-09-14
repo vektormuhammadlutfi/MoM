0157eadea06d29eb864277c566137a31a714fec6
KhairulAhyar22 <choirulahyar123@gmail.com>
2022-09-15 07:59:28 +0800

    login

--- resources/views/navbar/navuser.blade.php
+++ resources/views/navbar/navuser.blade.php
@@ -3,12 +3,12 @@
   <li class="nav-item dropdown">
     <a class="nav-link pr-0" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
       <div class="media align-items-center">
+        <div class="media-body mr-2 d-none d-lg-block">
+          <span class="mb-0 text-sm  font-weight-bold">Selamat Datang | ADMIN</span>
+        </div>
         <span class="avatar avatar-sm rounded-circle">
           <img alt="Image placeholder" src="{{URL::asset('/assets/img/theme/team-4-800x800.jpg')}}">
         </span>
-        <div class="media-body ml-2 d-none d-lg-block">
-          <span class="mb-0 text-sm  font-weight-bold">Admin</span>
-        </div>
       </div>
     </a>
     <div class="dropdown-menu dropdown-menu-arrow dropdown-menu-right">
