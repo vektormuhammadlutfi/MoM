6194b84d9574da585bd32f44cfcbedb7514ade54
KhairulAhyar22 <choirulahyar123@gmail.com>
2022-10-26 10:12:39 +0800

    mmperbaiki file composer.lock, filter crud di dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,46 +10,69 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        $tahun= 2022;
-        $jenismom = DB::table('tb_trans_moms')
-            ->Leftjoin('tb_trans_mom_details','tb_trans_moms.oid_mom','=','tb_trans_mom_details.oid_mom')
+        // $tahun= 2022;
+        $jenismom = DB::table('tb_mas_mom_jenis')
+            ->join('tb_trans_moms', 'tb_trans_moms.oid_jen_mom', '=', 'tb_mas_mom_jenis.oid_jen_mom')
+            ->join('tb_trans_mom_details', 'tb_trans_mom_details.oid_mom', '=','tb_trans_moms.oid_mom',)
+            ->where(function($query){
+                $query->where('tb_trans_mom_details.crud', 'C')
+                ->orWhere('tb_trans_mom_details.crud', 'U')
+                ->where(function($query){
+                    $query->where('tb_trans_moms.crud', 'C')
+                    ->orWhere('tb_trans_moms.crud', 'U');
+                });
+            })
+            ->where(function($query){
+                $query->where('tb_mas_mom_jenis.crud', 'U')
+                ->orWhere('tb_mas_mom_jenis.crud', 'C'); 
+            })
             ->select(
-                DB::raw('YEAR(tgl_mom) as Tahun'),
-                DB::raw("count(case when sts_issue = 'open' then 1 end) as open"),
-                DB::raw("count(case when sts_issue = 'progress' then 1 end) as progress"),
-                DB::raw("count(case when sts_issue = 'close' then 1 end) as close"),
-                DB::raw("count(case when sts_issue = 'hold' then 1 end) as hold"),
-                DB::raw("count(case when oid_jen_mom = 'JM-001' then 1 end) as Year"),
-                DB::raw("count(case when oid_jen_mom = 'JM-003' then 1 end) as Kuarta"),
-                DB::raw("count(case when oid_jen_mom = 'JM-004' then 1 end) as Monthly"),
-                DB::raw("count(case when oid_jen_mom = 'JM-002' then 1 end) as Weekly"),
+                DB::raw('YEAR(tb_trans_mom_details.created_at) as Tahun'),
+                DB::raw("count(case when tb_mas_mom_jenis.oid_jen_mom = 'JM-001' then 1 end) as Yearly"),
+                DB::raw("count(case when tb_mas_mom_jenis.oid_jen_mom = 'JM-003' then 1 end) as Kuartal"),
+                DB::raw("count(case when tb_mas_mom_jenis.oid_jen_mom = 'JM-004' then 1 end) as Monthly"),
+                DB::raw("count(case when tb_mas_mom_jenis.oid_jen_mom = 'JM-002' then 1 end) as Weekly"),
                 DB::raw("count(*) as Total"),
             )
-            ->groupBy('tahun')
+            ->groupBy('Tahun')
             ->get();
-        // dd($jenismom);
-        $totalStatus = DB::table('tb_trans_mom_details')
-            ->leftJoin('tb_trans_moms', 'tb_trans_mom_details.oid_mom', '=', 'tb_trans_moms.oid_mom')
-            ->leftJoin('tb_mas_mom_jenis', 'tb_trans_moms.oid_jen_mom', '=', 'tb_mas_mom_jenis.oid_jen_mom')
+
+        $totalStatus = DB::table('tb_mas_mom_jenis')
+            ->join('tb_trans_moms', 'tb_trans_moms.oid_jen_mom', '=', 'tb_mas_mom_jenis.oid_jen_mom')
+            ->join('tb_trans_mom_details', 'tb_trans_mom_details.oid_mom', '=','tb_trans_moms.oid_mom',)
+            ->where(function($query){
+                $query->where('tb_trans_mom_details.crud', 'C')
+                ->orWhere('tb_trans_mom_details.crud', 'U')
+                ->where(function($query){
+                    $query->where('tb_trans_moms.crud', 'C')
+                    ->orWhere('tb_trans_moms.crud', 'U');
+                });
+            })
+            ->where(function($query){
+                $query->where('tb_mas_mom_jenis.crud', 'U')
+                ->orWhere('tb_mas_mom_jenis.crud', 'C'); 
+            })
             ->select(
-                'jenis_mom',
-                DB::raw('count(*) as total'),
+                'tb_mas_mom_jenis.oid_jen_mom',
+                'tb_mas_mom_jenis.jenis_mom',
+                DB::raw('YEAR(tb_trans_mom_details.created_at) as Tahun'),
                 DB::raw('count(case when sts_issue = "Open" then 1 end) as open'),
                 DB::raw('count(case when sts_issue = "On Progress" then 1 end) as on_progress'),
                 DB::raw('count(case when sts_issue = "Hold" then 1 end) as hold'),
                 DB::raw('count(case when sts_issue = "Close" then 1 end) as close'),
             )
+            ->groupBy('Tahun')
+            ->groupBy('oid_jen_mom')
             ->groupBy('jenis_mom')
             ->get();
         // dd($totalStatus);
         return view('dashboard.dashboard', [
             'title' => 'Dashboard',
             'jenismom'=> $jenismom,
-            'totalstatus' => $totalStatus
+            'totalStatus' => $totalStatus
         ]);
     }
     public function weekly_status(){
-        $tahun = 2022;
         $weekly_status = DB::table('tb_mas_mom_jenis')
             ->join('tb_trans_moms', 'tb_mas_mom_jenis.oid_jen_mom', '=', 'tb_trans_moms.oid_jen_mom')
             ->join('tb_trans_mom_details', 'tb_trans_moms.oid_mom','=','tb_trans_mom_details.oid_mom')
@@ -72,7 +95,6 @@ class DashboardController extends Controller
         ]);
     }
     public function monthly_status(){
-        $tahun = 2022;
         $monthly_status = DB::table('tb_mas_mom_jenis')
             ->join('tb_trans_moms', 'tb_mas_mom_jenis.oid_jen_mom', '=', 'tb_trans_moms.oid_jen_mom')
             ->join('tb_trans_mom_details', 'tb_trans_moms.oid_mom','=','tb_trans_mom_details.oid_mom')
@@ -96,7 +118,6 @@ class DashboardController extends Controller
         ]);
     }
     public function kuartal_status(){
-        $tahun = 2022;
         $kuartal_status = DB::table('tb_mas_mom_jenis')
             ->join('tb_trans_moms', 'tb_mas_mom_jenis.oid_jen_mom', '=', 'tb_trans_moms.oid_jen_mom')
             ->join('tb_trans_mom_details', 'tb_trans_moms.oid_mom','=','tb_trans_mom_details.oid_mom')
@@ -119,7 +140,6 @@ class DashboardController extends Controller
         ]);
     }
     public function yearly_status(){
-        $tahun = 2022;
         $yearly_status = DB::table('tb_mas_mom_jenis')
             ->join('tb_trans_moms', 'tb_mas_mom_jenis.oid_jen_mom', '=', 'tb_trans_moms.oid_jen_mom')
             ->join('tb_trans_mom_details', 'tb_trans_moms.oid_mom','=','tb_trans_mom_details.oid_mom')
--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -6,76 +6,74 @@
 <div data-aos="fade-up" class="header bg-body mt--150 px-4">
 
   <div class="row-grid mb-4">
-    @foreach($jenismom as $item)
-      <div class="card">
-        <a href="/dashboard/weekly">
-          <div class="card-body p-3 hov-card">
-            <div class="row">
-              <div class="col">
-                <h3 class="card-title mb-1 hov-title">Weekly BoD</h3>   
-                <span class="h2 mb-0 font-weight-bolder body-number">{{$item->Weekly}}</span>
-              </div>
-              <div class="col-auto">
-                <div class="icon icon-shape bg-primary text-white rounded-circle shadow">
-                  <iconify-icon icon="bi:bar-chart-line-fill" style="font-size: 22px"></iconify-icon>
-                </div>
-              </div>
+    <div class="card">
+      <a href="/dashboard/weekly">
+        <div class="card-body p-3 hov-card">
+          <div class="row">
+            <div class="col">
+              <h3 class="card-title mb-1 hov-title">Weekly BoD</h3>   
+              <span id="weekly" class="h2 mb-0 font-weight-bolder body-number weekly"></span>
             </div>
-          </div>               
-        </a>        
-      </div>
-      <div class="card">
-        <a href="/dashboard/monthly">
-          <div class="card-body p-3 hov-card">
-            <div class="row">
-              <div class="col">
-                <h3 class="card-title mb-1 hov-title">PDCA Month KG</h3> 
-                <span class="h2 mb-0 font-weight-bolder body-number">{{$item->Monthly}}</span>
-              </div>
-              <div class="col-auto">
-                <div class="icon icon-shape bg-danger text-white rounded-circle shadow">
-                  <iconify-icon icon="bi:bar-chart-line-fill" style="font-size: 22px"></iconify-icon>
-                </div>
+            <div class="col-auto">
+              <div class="icon icon-shape bg-primary text-white rounded-circle shadow">
+                <iconify-icon icon="bi:bar-chart-line-fill" style="font-size: 22px"></iconify-icon>
               </div>
             </div>
           </div>
-        </a>
-      </div>
-      <div class="card">
-        <a href="/dashboard/kuartal">
-          <div class="card-body p-3 hov-card">
-            <div class="row">
-              <div class="col">
-                <h3 class="card-title mb-1 hov-title">Kuartal Meeting</h3>
-                <span class="h2 mb-0 font-weight-bolder body-number">{{$item->Kuarta}}</span>
-              </div>
-              <div class="col-auto">
-                <div class="icon icon-shape text-white rounded-circle shadow" style="background-color: rgb(241, 197, 0)">
-                  <iconify-icon icon="bi:bar-chart-line-fill" style="font-size: 22px"></iconify-icon>
-                </div>
+        </div>               
+      </a>        
+    </div>
+    <div class="card">
+      <a href="/dashboard/monthly">
+        <div class="card-body p-3 hov-card">
+          <div class="row">
+            <div class="col">
+              <h3 class="card-title mb-1 hov-title">PDCA Month KG</h3> 
+              <span id="monthly" class="h2 mb-0 font-weight-bolder body-number"></span>
+            </div>
+            <div class="col-auto">
+              <div class="icon icon-shape bg-danger text-white rounded-circle shadow">
+                <iconify-icon icon="bi:bar-chart-line-fill" style="font-size: 22px"></iconify-icon>
               </div>
             </div>
           </div>
-        </a>
-      </div>
-      <div class="card">
-        <a href="/dashboard/yearly">
-          <div class="card-body p-3 hov-card">
-            <div class="row">
-              <div class="col">                  
-                <h3 class="card-title mb-1 hov-title">End Year Meeting</h3>
-                <span class="h2 mb-0 font-weight-bolder body-number">{{$item->Year}}</span>
-              </div>
-              <div class="col-auto">
-                <div class="icon icon-shape bg-success text-white rounded-circle shadow">
-                  <iconify-icon icon="bi:bar-chart-line-fill" style="font-size: 22px"></iconify-icon>
-                </div>
+        </div>
+      </a>
+    </div>
+    <div class="card">
+      <a href="/dashboard/kuartal">
+        <div class="card-body p-3 hov-card">
+          <div class="row">
+            <div class="col">
+              <h3 class="card-title mb-1 hov-title">Kuartal Meeting</h3>
+              <span id="kuartal" class="h2 mb-0 font-weight-bolder body-number"></span>
+            </div>
+            <div class="col-auto">
+              <div class="icon icon-shape text-white rounded-circle shadow" style="background-color: rgb(241, 197, 0)">
+                <iconify-icon icon="bi:bar-chart-line-fill" style="font-size: 22px"></iconify-icon>
               </div>
             </div>
           </div>
-        </a>
-      </div>
-    @endforeach
+        </div>
+      </a>
+    </div>
+    <div class="card">
+      <a href="/dashboard/yearly">
+        <div class="card-body p-3 hov-card">
+          <div class="row">
+            <div class="col">                  
+              <h3 class="card-title mb-1 hov-title">End Year Meeting</h3>
+              <span id="yearly" class="h2 mb-0 font-weight-bolder body-number"></span>
+            </div>
+            <div class="col-auto">
+              <div class="icon icon-shape bg-success text-white rounded-circle shadow">
+                <iconify-icon icon="bi:bar-chart-line-fill" style="font-size: 22px"></iconify-icon>
+              </div>
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
   </div>
   <div class="row-grid-chart">
       <!-- pie chart -->
@@ -109,17 +107,23 @@
 </div>
 
 @endsection
+
+@push('addon-script')
 <script src="./assets/js/plugins/chart.js/dist/Chart.min.js"></script>
 <script src="https://canvasjs.com/assets/script/canvasjs.min.js"> </script>
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-
 <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 
-<!-- new -->
-{{-- <script src='https://cdn.plot.ly/plotly-2.14.0.min.js'></script> --}}
-
-
-
+{{-- script card jenis mom --}}
+<script>
+  var jenisMoms = {!! $jenismom !!}
+  jenisMoms.filter((datafilter) => datafilter.Tahun == 2022).map((dataloop) => {
+    document.getElementById("weekly").innerText = `${dataloop.Weekly}`;
+    document.getElementById("monthly").innerText = `${dataloop.Monthly}`;
+    document.getElementById("kuartal").innerText = `${dataloop.Kuartal}`;
+    document.getElementById("yearly").innerText = `${dataloop.Yearly}`;
+  })
+</script>
 
 <!-- pie chart settings -->
 <script type="text/javascript">
@@ -127,96 +131,88 @@
   google.charts.setOnLoadCallback(drawChart);
 
   function drawChart() {
-      @foreach($jenismom as $item)
-          var weekly = parseInt(<?php echo $item->Weekly; ?>);
-          var monthly = parseInt(<?php echo $item->Monthly; ?>);
-          var kuartal = parseInt(<?php echo $item->Kuarta; ?>);
-          var yearly = parseInt(<?php echo $item->Year; ?>);
-      @endforeach
-  var data = google.visualization.arrayToDataTable([
+    var jenisMoms = {!! $jenismom !!};
+    var weekly = 0; var monthly = 0; var kuartal = 0; var yearly = 0
+    jenisMoms.filter((data) => data.Tahun == 2022).map((dataloop) => {
+      weekly = dataloop.Weekly;
+      monthly = dataloop.Monthly;
+      kuartal = dataloop.Kuartal;
+      yearly = dataloop.Yearly;
+    });
+    // console.log(weekly, monthly, kuartal, yearly);
+    var data = google.visualization.arrayToDataTable([
       ['Task', 'Meeting Progress'],
       ['Weekly BoD',     weekly],
       ['PDCA Month',      monthly],
       ['Kuartal Meeting',  kuartal],
       ['End Year Meeting', yearly]
-  ]);
+    ]);
 
-  var options = {
-      title: ''
-  };
+    var options = {
+        title: ''
+    };
 
-  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-  chart.draw(data, options);
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+    chart.draw(data, options);
   }
 </script>
 
-<!-- bar chart settings -->
-  <script type="text/javascript">
-    google.charts.load('current', {'packages':['bar']});
-    google.charts.setOnLoadCallback(drawChart);
+// bar chart settings
+<script type="text/javascript">
+  google.charts.load('current', {'packages':['bar']});
+  google.charts.setOnLoadCallback(drawChart);
 
-    function drawChart() {
-      // variabel default total status jenmom 
-      let yearopen = 0; let yearonprogress = 0; let yearhold = 0; let yearclose = 0;
-      let kuartalopen = 0; let kuartalonprogress = 0; let kuartalhold = 0; let kuartalclose = 0;
-      let monthopen = 0; let monthonprogress = 0; let monthhold = 0; let monthclose = 0;
-      let weekopen = 0; let weekonprogress = 0; let weekhold = 0; let weekclose = 0;
+  function drawChart() {
+    // variabel default total status jenmom 
+    let yearopen = 0; let yearonprogress = 0; let yearhold = 0; let yearclose = 0;
+    let weekopen = 0; let weekonprogress = 0; let weekhold = 0; let weekclose = 0;
+    let kuartalopen = 0; let kuartalonprogress = 0; let kuartalhold = 0; let kuartalclose = 0;
+    let monthopen = 0; let monthonprogress = 0; let monthhold = 0; let monthclose = 0;
 
-        @foreach($totalstatus as $status)
-          @if($status->jenis_mom == 'End Year Meeting 2022')
-            yearopen = <?php echo $status->open; ?>;
-            yearonprogress = <?php echo $status->on_progress; ?>;
-            yearhold = <?php echo $status->hold; ?>;
-            yearclose = <?php echo $status->close; ?>;
-          @endif          
-          @if($status->jenis_mom == 'Kuartal Meeting')
-            kuartalopen = <?php echo $status->open; ?>;
-            kuartalonprogress = <?php echo $status->on_progress; ?>;
-            kuartalhold = <?php echo $status->hold; ?>;
-            kuartalclose = <?php echo $status->close; ?>;
-          @endif          
-          @if($status->jenis_mom == 'PDCA Month KGsd')
-            monthopen = <?php echo $status->open; ?>;
-            monthonprogress = <?php echo $status->on_progress; ?>;
-            monthhold = <?php echo $status->hold; ?>;
-            monthclose = <?php echo $status->close; ?>;
-          @endif          
-          @if($status->jenis_mom == 'Weekly BoD')
-            weekopen = <?php echo $status->open; ?>;
-            weekonprogress = <?php echo $status->on_progress; ?>;
-            weekhold = <?php echo $status->hold; ?>;
-            weekclose = <?php echo $status->close; ?>;
-          @endif          
-        @endforeach
+    var totalStatus = {!! $totalStatus !!};
+    var jen_year = totalStatus.filter((e) => e.Tahun == 2022 && e.oid_jen_mom == 'JM-001');
+    var jen_week = totalStatus.filter((e) => e.Tahun == 2022 && e.oid_jen_mom == 'JM-002');
+    var jen_kuartal = totalStatus.filter((e) => e.Tahun == 2022 && e.oid_jen_mom == 'JM-003');
+    var jen_month = totalStatus.filter((e) => e.Tahun == 2022 && e.oid_jen_mom == 'JM-004');
 
-      console.log(`nilai onpress : ${yearonprogress}`)
-      console.log(`nilai open : ${yearopen}`)
-      console.log(`nilai close : ${yearclose}`)
-      console.log(`nilai hold : ${yearhold}`)
-      
-      var data = google.visualization.arrayToDataTable([
-        ['Status', 'Weekly BoD', 'PDCA Month KG', 'Kuartal Meeting','End Year Meeting'],
-        ['Open', weekopen , monthopen, kuartalopen, yearopen],
-        ['Hold', weekhold , monthhold, kuartalhold, yearhold],
-        ['On Progress', weekonprogress , monthonprogress, kuartalonprogress, yearonprogress],
-        ['Close', weekclose , monthclose, kuartalclose, yearclose]
-        // ['Open', 1, 2 ,5, 1],
-        // ['Hold', 1, 2 ,5, 1],
-        // ['On Progress', 1, 2 ,5, 1],
-        // ['Close', 1, 2 ,5, 1]
-      ]);
+    jen_year.map((e) => {
+      yearopen = e.open; yearonprogress = e.on_progress; yearhold = e.hold; yearclose = e.close;
+    });
+    jen_week.map((e) => {
+      weekopen = e.open; weekonprogress = e.on_progress; weekhold = e.hold; weekclose = e.close;
+    });
+    jen_kuartal.map((e) => {
+      kuartalopen = e.open; kuartalonprogress = e.on_progress; kuartalhold = e.hold; kuartalclose = e.close;
+    });
+    jen_month.map((e) => {
+      monthopen = e.open; monthonprogress = e.on_progress; monthhold = e.hold; monthclose = e.close;
+    });
+     
+    console.log(`nilai onpress : ${weekonprogress}`)
+    console.log(`nilai open : ${weekopen}`)
+    console.log(`nilai close : ${weekclose}`)
+    console.log(`nilai hold : ${weekhold}`)
 
-      var options = {
-        chart: {
-          title: '',
-        },
-        bars: 'vertical' // Required for Material Bar Charts.
-      };
+    var data = google.visualization.arrayToDataTable([
+      ['Status', 'Weekly BoD', 'PDCA Month KG', 'Kuartal Meeting','End Year Meeting'],
+      ['Open', weekopen , monthopen, kuartalopen, yearopen],
+      ['Hold', weekhold , monthhold, kuartalhold, yearhold],
+      ['On Progress', weekonprogress , monthonprogress, kuartalonprogress, yearonprogress],
+      ['Close', weekclose , monthclose, kuartalclose, yearclose]
+    ]);
 
-      var chart = new google.charts.Bar(document.getElementById('barchart_material'));
+    var options = {
+      chart: {
+        title: '',
+      },
+      bars: 'vertical' // Required for Material Bar Charts.
+    };
 
-      chart.draw(data, google.charts.Bar.convertOptions(options));
-    }
-  </script>
+    var chart = new google.charts.Bar(document.getElementById('barchart_material'));
+
+    chart.draw(data, google.charts.Bar.convertOptions(options));
+  }
+</script>
+@endpush
+
 
